013db2cc5f50fc8e804b48b7dc447239df4ad0b8
maksymopanasenko <maxpanas008@gmail.com>
2024-01-06 23:22:27 +0100

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,72 @@
+import { Link } from 'react-scroll';
+import styles from './Header.module.scss';
+import Container from '../Container/Container';
+import logo from './icons/sign.webp';
+
+const Header = () => {
+    return (
+        <header className={styles.Header}>
+            <Container>
+                <div className={styles.HeaderBody}>
+                    <Link
+                        className={styles.HeaderLink}
+                        to="/"
+                        duration={500}
+                    >
+                        <span className={styles.HeaderLogo}>
+                            4
+                            <img src={logo} alt='logo' width={55} />
+                            7
+                        </span>
+                    </Link>
+                    <nav className={styles.HeaderNav}>
+                        <ul className={styles.HeaderList}>
+                            <li className={styles.HeaderListItem}>
+                                <Link
+                                    className={styles.HeaderLink}
+                                    to="#"
+                                    duration={500}
+                                    aria-label="#"
+                                >
+                                    Text1
+                                </Link>
+                            </li>
+                            <li className={styles.HeaderListItem}>
+                                <Link
+                                    className={styles.HeaderLink}
+                                    to="#"
+                                    duration={500}
+                                    aria-label="#"
+                                >
+                                    Text2
+                                </Link>
+                            </li>
+                            <li className={styles.HeaderListItem}>
+                                <Link
+                                    className={styles.HeaderLink}
+                                    to="#"
+                                    duration={500}
+                                    aria-label="#"
+                                >
+                                    Text3
+                                </Link>
+                            </li>
+                            <li className={styles.HeaderListItem}>
+                                <Link
+                                    className={styles.HeaderLink}
+                                    to="#"
+                                    duration={500}
+                                    aria-label="#"
+                                >
+                                    Shooting range
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </Container>
+        </header>
+    );
+};
+
+export default Header;
